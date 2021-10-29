0a89403680ad3aaebf0bd96434dcbf105a9fcff8
owingsbj <owingsbj@yahoo.com>
2021-10-29 13:44:02 -0700

    Hide navigation when in full screen

--- app/src/main/java/com/gallantrealm/modsynth/MainActivity.java
+++ app/src/main/java/com/gallantrealm/modsynth/MainActivity.java
@@ -164,6 +164,16 @@ public class MainActivity extends Activity implements View.OnClickListener, Clie
 
 		setContentView(R.layout.synth_screen);
 
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+				// Set the content to appear under the system bars so that the
+				// content doesn't resize when the system bars hide and show.
+				| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				// Hide the nav bar and status bar
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_FULLSCREEN);
+
 		// Set sustained performance mode (Android 7.0+)
 		if (Build.VERSION.SDK_INT >= 24) {
 			getWindow().setSustainedPerformanceMode(true);
