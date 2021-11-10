09a7e5e7cae2502c20b6ac2eb3f36e8bec223a6b
owingsbj <owingsbj@yahoo.com>
2021-11-09 21:30:44 -0800

    Add window focus handler to re-hide navigation

--- app/src/main/java/com/gallantrealm/modsynth/MainActivity.java
+++ app/src/main/java/com/gallantrealm/modsynth/MainActivity.java
@@ -1348,6 +1348,21 @@ public class MainActivity extends Activity implements View.OnClickListener, Clie
 	}
 
 	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		if (hasFocus) {
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+					// Set the content to appear under the system bars so that the
+					// content doesn't resize when the system bars hide and show.
+					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					// Hide the nav bar and status bar
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_FULLSCREEN);
+		}
+	}
+
+	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 	}
